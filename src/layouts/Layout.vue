d657624cd51c3a0c346c22b0047378fcb0f7edd0
mel0620 <cunetarommel0620@gmail.com>
2019-10-15 13:57:14 +0800

    trenmlm

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -67,6 +67,23 @@
                     text-transform: uppercase;
                     position: relative;
                     box-sizing: border-box;
+                    transition: .3s all ease-in-out;
+
+                    &::after {
+                        transition: .3s all ease-in-out;
+                    }
+
+                    &:hover {
+                        &::after {
+                            content: '';
+                            background-color: rgba(#fff, .5);
+                            height: 2px;
+                            position: absolute;
+                            bottom: 0;
+                            left: 0;
+                            right: 0;
+                        }
+                    }
 
                     &.active {
                         &::after {
