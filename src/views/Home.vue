6ad9b05d1751ac1b6b8e970c3cdd3fde6fc45472
Rommel <cunetarommel0620@gmail.com>
2019-10-17 10:39:25 +0800

    services

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
   <div class="home-page">
     <hero></hero>
     <services></services>
+    <works></works>
   </div>
 </template>
 
 <script>
 import Hero from './HomeSections/Hero.vue'
 import Services from './HomeSections/Services.vue'
+import Works from './HomeSections/Works.vue'
 
 export default {
   name: 'home',
   components: {
     Hero,
-    Services
+    Services,
+    Works
   }
 }
 </script>
---
+++ src/views/HomeSections/Works.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="works-wrapper">
+        <div class="container">
+            <div class="works">
+                <div class="works-header">
+                    <div class="works-header__1">
+                        WORKS
+                    </div>
+                    <div class="works-header__2">
+                        Our Recent and Newest Project
+                    </div>
+                    <div class="works-border"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss">
+
+    .works-wrapper
+    {
+        background-color: #007eac;
+        padding: 100px 0px;
+    }
+
+</style>
