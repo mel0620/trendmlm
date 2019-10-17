9a56032994bdb04cc88c63283b40c69781c1da28
Rommel <cunetarommel0620@gmail.com>
2019-10-17 11:48:34 +0800

    jam

--- src/views/HomeSections/Works.vue
+++ src/views/HomeSections/Works.vue
@@ -12,7 +12,52 @@
                     <div class="section-title__border"></div>
                 </div>
                 <div class="works-content">
-
+                    <div class="works-swiper">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <div class="works-image">
+                                    <div class="image-holder">
+                                        <img src="../../assets/works-1.png" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="works-image">
+                                    <div class="image-holder">
+                                        <img src="../../assets/works-2.png" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="works-image">
+                                    <div class="image-holder">
+                                        <img src="../../assets/works-3.png" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="works-image">
+                                    <div class="image-holder">
+                                        <img src="../../assets/works-4.png" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="works-image">
+                                    <div class="image-holder">
+                                        <img src="../../assets/works-5.png" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="works-image">
+                                    <div class="image-holder">
+                                        <img src="../../assets/works-6.png" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -31,27 +76,29 @@ export default {
     },
     methods: {
         worksSwiper() {
-            var mySwiper = new Swiper ('.swiper-container', {
-                // Optional parameters
-                // direction: 'vertical',
-                loop: true,
-
-                // If we need pagination
-                pagination: {
-                el: '.swiper-pagination',
+            var swiper = new Swiper('.works-swiper', {
+            slidesPerView: 3,
+            slidesPerColumn: 2,
+            spaceBetween: 20,
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction: false,
+            },
+            breakpoints: {
+                        768: {
+                            slidesPerView: 3,
+                        },
+                        425: {
+                            slidesPerView: 2,
+                        },
+                        375: {
+                            slidesPerView: 2,
                 },
-
-                // Navigation arrows
-                navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-                },
-
-                // And if we need scrollbar
-                // scrollbar: {
-                // el: '.swiper-scrollbar',
-                // },
-            })
+                320: {
+                            slidesPerView: 1,
+                        }
+                    }
+            });
         }
     },
     mounted() {
@@ -63,14 +110,15 @@ export default {
 <style lang="scss">
 @import "../../scss/style";
 
-    .works-wrapper
-    {
+    .works-wrapper {
         background-color: #007eac;
         padding: 100px 0px;
+
         .works-header {
             margin-bottom: 50px;
             text-align: center;
             line-height: 2rem;
+
             .works-header__1 {
                 font-family: $font-primary;
                 font-size: 18px;
@@ -89,6 +137,7 @@ export default {
                 height: 3px;
                 position: relative;
                 margin: 15px auto;
+
                 &:before {
                     content: '';
                     background-color: #fff;
@@ -110,6 +159,54 @@ export default {
                     transform: translateY(-50%);
                 }
             }
+        }
+
+        .works-content {
+
+            .works-swiper {
+				width: 100%;
+				height: 100%;
+                overflow: hidden;
+
+				.swiper-wrapper {
+
+					.swiper-slide {
+
+						.works-image {
+
+							position: relative;
+							width: 100%;
+							background-color: #fff;
+                            cursor: pointer;
+
+							.image-holder {
+								text-align: center;
+								width: 100%;
+								overflow: hidden;
+								@media screen and (max-width: 425px)
+								{
+									padding: 10px;
+								}
+								img {
+									width: 100%;
+									margin: auto;
+									object-fit: cover;
+									object-position: center;
+									height: 250px;
+									@media screen and (max-width: 425px)
+									{
+										width: 200px;
+									}
+									@media screen and (max-width: 320px)
+									{
+										width: 250px;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
         }
     }
 
